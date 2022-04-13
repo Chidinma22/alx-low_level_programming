66cc8bcb77b452d241f824a8b969edcfccb4c9db
Chidinma22 <oc.odiegwu@stu.unizik.edu.ng>
2022-04-13 15:23:00 -0700

    putchar commit message

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *program starts from main function
+ *
+ *return  : 0
+ **/
+int main(void)
+{
+{
+int putchar(int "_");
+int putchar(int "p");
+int putchat(int "u");
+int putchar(int "t");
+int putchar(int "c");
+int putchar(int "h");
+int putchar(int "a");
+int putchar(int "r");
+int putchar(int "\n");
+}
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this program prints _putchar followed by a new line
